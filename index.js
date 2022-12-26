f7b5823055d854014d4ed2d9a8ef987fc11d2af4
Sarah Al-Dhaferi <55980263+SarahSalem-hub@users.noreply.github.com>
2022-12-26 16:10:31 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+
+var customerName = "bob"
+var bestCustomer 
+const leastFavoriteCustomer ="maybe bob"
+
+
+function upperCaseCustomerName(){
+    return customerName = customerName.toUpperCase()
+}
+function setBestCustomer( ){
+    return bestCustomer = 'not bob'
+}
+function overwriteBestCustomer(modify){
+    bestCustomer = modify
+}
+function changeLeastFavoriteCustomer(){
+    try {
+        return leastFavoriteCustomer = "f"
+    } catch (error) {
+        throw error
+    }
+}
+
+console.log(changeLeastFavoriteCustomer())
